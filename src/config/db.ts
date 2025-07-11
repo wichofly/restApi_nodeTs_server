f758cf07867bbf2b667f5b6d44599ad8a7984f0d
Mauricio Erazo <antonierazo13@hotmail.com>
2025-07-11 22:16:59 +0200

    feat: add dotenv for environment variable management and update database connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,15 +1,15 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const db = new Sequelize(
-  'postgresql://rest_api_node_typescript_fmlp_user:xWLNVBBQnlkgAgEhgvMzlyYbWPsbQRzi@dpg-d1om0k3uibrs73cun900-a.oregon-postgres.render.com/rest_api_node_typescript_fmlp',
-  {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // Allow self-signed certificates (Render usually requires this)
-      },
+dotenv.config();
+
+const db = new Sequelize(process.env.DATABASE_URL!, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Allow self-signed certificates (Render usually requires this)
     },
-  }
-);
+  },
+});
 
 export default db;
