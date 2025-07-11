1ffa2c3e6f8075773d2411c6305104927e638d0c
Mauricio Erazo <antonierazo13@hotmail.com>
2025-07-11 19:42:36 +0200

    feat: implement basic CRUD routes in router

---
+++ src/router.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+
+const router = Router();
+
+// Route example
+router.get('/', (req, res) => {
+  // res.json(data);
+  res.send('from Get');
+});
+
+router.post('/', (req, res) => {
+  // res.json(data);
+  res.send('from Post');
+});
+
+router.put('/', (req, res) => {
+  // res.json(data);
+  res.send('from Put');
+});
+
+router.patch('/', (req, res) => {
+  // res.json(data);
+  res.send('from Patch');
+});
+
+router.delete('/', (req, res) => {
+  // res.json(data);
+  res.send('from Delete');
+});
+
+export default router;
