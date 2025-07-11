09fc9103171afa98c0bf9380fe091bcbfa3698ca
Mauricio Erazo <antonierazo13@hotmail.com>
2025-07-11 16:37:56 +0200

    first commit

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+import { sum } from './server';
+console.log('Hello World!');
+sum(3, 5);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { sum } from './server';
+
+console.log('Hello World!');
+
+console.log(sum(3, 5));
