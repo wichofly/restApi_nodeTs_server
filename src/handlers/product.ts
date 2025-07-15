ed919f0f276a82e2adb418f172ad6090a3747488
Mauricio Erazo <antonierazo13@hotmail.com>
2025-07-15 20:31:56 +0200

    feat: move product validation to router and clean up createProduct handler

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,19 +1,8 @@
 import { Request, Response } from 'express';
-import { check, validationResult } from 'express-validator';
+import { validationResult } from 'express-validator';
 import Product from '../models/Product.model';
 
 export const createProduct = async (req: Request, res: Response) => {
-  // Validation
-  await check('name').notEmpty().withMessage('Name is required').run(req);
-  await check('price')
-    .isNumeric()
-    .withMessage('Price must be a number')
-    .custom((value) => value > 0)
-    .withMessage('Price must be greater than zero')
-    .notEmpty()
-    .withMessage('Price is required')
-    .run(req);
-
   let errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
+
 import { createProduct } from './handlers/product';
 
 const router = Router();
@@ -9,7 +11,21 @@ router.get('/', (req, res) => {
   res.send('from Get');
 });
 
-router.post('/', createProduct);
+router.post(
+  '/',
+
+  // Validation
+  body('name').notEmpty().withMessage('Name is required'),
+  body('price')
+    .isNumeric()
+    .withMessage('Price must be a number')
+    .custom((value) => value > 0)
+    .withMessage('Price must be greater than zero')
+    .notEmpty()
+    .withMessage('Price is required'),
+
+  createProduct
+);
 
 router.put('/', (req, res) => {
   // res.json(data);
