140c6890a1d1f8ed59ade9d5d3996f5a654a5dad
Mauricio Erazo <antonierazo13@hotmail.com>
2025-07-16 12:31:02 +0200

    refactor: update Product model to use declare syntax for properties

--- src/models/Product.model.ts
+++ src/models/Product.model.ts
@@ -8,20 +8,20 @@ class Product extends Model<Product> {
   @Column({
     type: DataType.STRING(100),
   })
-  name!: string;
+  declare name: string;
 
   @Column({
     type: DataType.FLOAT,
     allowNull: false,
   })
-  price!: number;
+  declare price: number;
 
   @Default(true)
   @Column({
     type: DataType.BOOLEAN,
     allowNull: false,
   })
-  availability!: number;
+  declare availability: number;
 }
 
 export default Product;
