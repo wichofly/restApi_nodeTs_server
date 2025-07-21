8dd78274da357c4285e4a25eb5b81db5b69d6bc1
Mauricio Erazo <antonierazo13@hotmail.com>
2025-07-21 18:21:09 +0200

    feat: add initial test cases and update tsconfig for isolated modules

---
+++ src/__tests__/server.test.ts
@@ -0,0 +1,13 @@
+describe('testing the first test', () => {
+  test(' 1 + 1 should equal 2', () => {
+    expect(1 + 3).toBe(4);
+  });
+
+  test('Not equal 3', () => {
+    expect(2 + 2).not.toBe(3);
+  } )
+});
+
+/**
+ * test() and it() are the same.
+ */
