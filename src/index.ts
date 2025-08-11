111f618b46a780c576c45ac194030e02f93c26c9
Mauricio Erazo <antonierazo13@hotmail.com>
2025-08-11 23:34:29 +0200

    fix: update server port to 5000 for consistency - new url in render was made. - needed to update the port number to work on postman and DBeaver

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import colors from 'colors';
 import server from './server';
 
-const port = process.env.PORT || 4000;
-server.listen(4000, () => {
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
   console.log(colors.cyan.bold(`Server is running on port ${port}`));
 });
