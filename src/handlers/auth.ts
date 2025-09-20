9b5403111caf0e29a786ab015f3a1dc5210538d4
Mauricio Erazo <antonierazo13@hotmail.com>
2025-09-20 17:17:11 +0200

    refactor: clean up whitespace and enhance login response with user info

--- src/handlers/auth.ts
+++ src/handlers/auth.ts
@@ -2,13 +2,12 @@ import { Request, Response } from 'express';
 import User from '../models/User.model';
 import jwt from 'jsonwebtoken';
 
-
 export const register = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
   const existingUser = await User.findOne({ where: { email } });
   if (existingUser)
     return res.status(400).send({ error: 'User already exists' });
-  
+
   const user = new User({ name, email, password });
   await user.hashPassword();
   await user.save();
@@ -21,12 +20,16 @@ export const login = async (req: Request, res: Response) => {
   if (!user || !(await user.checkPassword(password))) {
     return res.status(401).send({ error: 'Invalid email or password' });
   }
-  
+
   const JWT_SECRET = process.env.JWT_SECRET!;
-  
+
   // Generate JWT
   const token = jwt.sign({ id: user.id, email }, JWT_SECRET, {
     expiresIn: '1h',
   });
-  res.send({ data: { token } });
+
+  // Send token in response along with user info
+  res.send({
+    data: { token, user: { id: user.id, name: user.name, email: user.email } },
+  });
 };
